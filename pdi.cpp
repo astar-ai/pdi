670e4fbf3b1946e3d25de189dd410d0ef178b9e6
Astar.ai <astar.ai.inc@gmail.com>
2019-06-01 21:58:05 -0700

    Update pdi.cpp

--- pdi.cpp
+++ pdi.cpp
@@ -5,28 +5,13 @@
 
 #include <GL/gl.h>
 #include <opencv2/opencv.hpp>
-
 #include "glwindow/scenewindow.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 
-bool      live = false;
-//To run 'live = true' mode, you need a CaliCam from www.astar.ai
-
-int       rect_cols = 640, rect_rows = 480;
-int       ndisp_bar =  1,  wsize_bar = 2, thr_bar = 0;
-int       ndisp_max =  2,  wsize_max = 3, thr_max = 120;
-int       ndisp_now = 32,  wsize_now = 9, thr_now = 200;
-
-int       cap_cols, cap_rows, img_width;
-double    xi;
-
-cv::Mat   Translation, Kl, Kr, Dl, Dr, xil, xir, Rl, Rr,
-          mmap[2], smap[2][2], Kfe;
-
-std::string cam_model;
-
-glwindow::SceneWindow* scene;
+int ndisp_bar = 1,   wsize_bar = 2, thr_bar = 0;
+int ndisp_max = 2,   wsize_max = 3, thr_max = 130;
+int ndisp_now = 32,  wsize_now = 9, thr_now = 190;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -43,10 +28,14 @@ void OnTrackWsize(int, void*) {
 ////////////////////////////////////////////////////////////////////////////////
 
 void OnTrackThreshold(int, void*) {
-  thr_now = 200 + thr_bar;
+  thr_now = 190 + thr_bar;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+
+int     cap_cols, cap_rows, img_width;
+cv::Mat T, Kl, Kr, Dl, Dr, xil, xir, Rl, Rr;
+cv::Mat fmap[2], lmap[2][2], Kfe;
 
 void LoadParameters(std::string file_name) {
   cv::FileStorage fs(file_name, cv::FileStorage::READ);
@@ -56,46 +45,47 @@ void LoadParameters(std::string file_name) {
   }
 
   cv::Size cap_size;
-  fs["cam_model"] >> cam_model;
   fs["cap_size" ] >> cap_size;
   fs["Kl"       ] >> Kl;
   fs["Dl"       ] >> Dl;
   fs["xil"      ] >> xil;
-  Rl = cv::Mat::eye(3, 3, CV_64F);
-
-  if (cam_model == "stereo") {
-    fs["Rl"       ] >> Rl;
-    fs["Kr"       ] >> Kr;
-    fs["Dr"       ] >> Dr;
-    fs["xir"      ] >> xir;
-    fs["Rr"       ] >> Rr;
-    fs["T"        ] >> Translation;
-  }
+  fs["Rl"       ] >> Rl;
+  fs["Kr"       ] >> Kr;
+  fs["Dr"       ] >> Dr;
+  fs["xir"      ] >> xir;
+  fs["Rr"       ] >> Rr;
+  fs["T"        ] >> T;
   fs.release();
 
-  img_width = cap_size.width;
   cap_cols  = cap_size.width;
   cap_rows  = cap_size.height;
-
-  if (cam_model == "stereo")
-    img_width  = cap_size.width / 2;
+  img_width = cap_size.width / 2;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-enum RecMode {
-  REC_PERSPECTIVE,
-  REC_FISHEYE,
-  REC_LONGLAT
-};
-
-inline double MatRowMul(const cv::Mat& m, double x, double y, double z, int r) {
+inline double MatRowMul(cv::Mat m, double x, double y, double z, int r) {
   return m.at<double>(r,0) * x + m.at<double>(r,1) * y + m.at<double>(r,2) * z;
 }
 
-void InitRectifyMap(cv::Mat K, cv::Mat D, double xi0, cv::Mat R,
-                    cv::Mat Knew, double xi1, cv::Size size,
-                    cv::Mat& map1, cv::Mat& map2, RecMode mode) {
+////////////////////////////////////////////////////////////////////////////////
+
+enum RectMode {
+  RECT_PERSPECTIVE,
+  RECT_FISHEYE,
+  RECT_LONGLAT
+};
+
+void InitRectifyMap(cv::Mat K,
+                    cv::Mat D,
+                    cv::Mat R,
+                    cv::Mat Knew,
+                    double xi0,
+                    double xi1,
+                    cv::Size size,
+                    RectMode mode,
+                    cv::Mat& map1,
+                    cv::Mat& map2) {
   map1.create(size, CV_32F);
   map2.create(size, CV_32F);
 
@@ -120,13 +110,13 @@ void InitRectifyMap(cv::Mat K, cv::Mat D, double xi0, cv::Mat R,
       double yc = 0.;
       double zc = 0.;
 
-      if (mode == REC_PERSPECTIVE) {
+      if (mode == RECT_PERSPECTIVE) {
         xc = MatRowMul(KRi, c, r, 1., 0);
         yc = MatRowMul(KRi, c, r, 1., 1);
         zc = MatRowMul(KRi, c, r, 1., 2);
       }
 
-      if (mode == REC_LONGLAT) {
+      if (mode == RECT_LONGLAT) {
         double tt = MatRowMul(Ki, c, r, 1., 0); // w/pi
         double pp = MatRowMul(Ki, c, r, 1., 1); // h/pi
 
@@ -139,7 +129,7 @@ void InitRectifyMap(cv::Mat K, cv::Mat D, double xi0, cv::Mat R,
         zc = MatRowMul(Ri, xn, yn, zn, 2);
       }
 
-      if (mode == REC_FISHEYE) {
+      if (mode == RECT_FISHEYE) {
         double ee = MatRowMul(Ki, c, r, 1., 0);
         double ff = MatRowMul(Ki, c, r, 1., 1);
 
@@ -179,6 +169,8 @@ void InitRectifyMap(cv::Mat K, cv::Mat D, double xi0, cv::Mat R,
 
 ////////////////////////////////////////////////////////////////////////////////
 
+int rect_cols = 640, rect_rows = 480;
+
 void InitRectifyMap() {
   Kfe = cv::Mat::eye(3, 3, CV_64F);
   Kfe.at<double>(0,0) = Kl.at<double>(0,0) * 0.4;
@@ -186,30 +178,28 @@ void InitRectifyMap() {
   Kfe.at<double>(1,1) = Kfe.at<double>(0,0);
   Kfe.at<double>(1,2) = rect_rows / 2. - 0.5;
 
-  xi = xil.at<double>(0,0);
   cv::Size img_size(rect_cols, rect_rows);
-  InitRectifyMap(Kl, Dl, xil.at<double>(0,0), Rl, Kfe, xil.at<double>(0,0),
-                 img_size, mmap[0], mmap[1], REC_FISHEYE);
-
-  cv::Mat Kll = cv::Mat::eye(3, 3, CV_64F);
+  cv::Mat  Kll = cv::Mat::eye(3, 3, CV_64F);
   Kll.at<double>(0,0) = img_size.width  / M_PI;
   Kll.at<double>(1,1) = img_size.height / M_PI;
 
-  InitRectifyMap(Kl, Dl, xil.at<double>(0,0), Rl, Kll, 0.,
-                 img_size, smap[0][0], smap[0][1], REC_LONGLAT);
-  InitRectifyMap(Kr, Dr, xir.at<double>(0,0), Rr, Kll, 0.,
-                 img_size, smap[1][0], smap[1][1], REC_LONGLAT);
+  InitRectifyMap(Kl, Dl, Rl, Kfe, xil.at<double>(0,0), xil.at<double>(0,0),
+                 img_size, RECT_FISHEYE, fmap[0], fmap[1]);
+  InitRectifyMap(Kl, Dl, Rl, Kll, xil.at<double>(0,0), 0.,
+                 img_size, RECT_LONGLAT, lmap[0][0], lmap[0][1]);
+  InitRectifyMap(Kr, Dr, Rr, Kll, xir.at<double>(0,0), 0.,
+                 img_size, RECT_LONGLAT, lmap[1][0], lmap[1][1]);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void DisparityImage(const cv::Mat& recl, const cv::Mat& recr, cv::Mat& disp) {
-  cv::Mat disp16s;
+void DisparityImage(const cv::Mat& recl, const cv::Mat& recr, cv::Mat& dispf) {
+  cv::Mat disps;
   int N = ndisp_now, W = wsize_now, C = recl.channels();
   if (1) {
     cv::Ptr<cv::StereoSGBM> sgbm =
         cv::StereoSGBM::create(0, N, W, 8 * C * W * W, 32 * C * W * W);
-    sgbm->compute(recl, recr, disp16s);
+    sgbm->compute(recl, recr, disps);
   } else {
     cv::Mat grayl, grayr;
     cv::cvtColor(recl, grayl, CV_BGR2GRAY);
@@ -223,22 +213,28 @@ void DisparityImage(const cv::Mat& recl, const cv::Mat& recr, cv::Mat& disp) {
     sbm->setSpeckleWindowSize(100);
     sbm->setSpeckleRange(32);
     sbm->setDisp12MaxDiff(1);
-    sbm->compute(grayl, grayr, disp16s);
+    sbm->compute(grayl, grayr, disps);
   }
 
-  disp16s.convertTo(disp, CV_32F, 1.f / 16.f);
+  disps.convertTo(dispf, CV_32F, 1.f / 16.f);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void DrawScene(const cv::Mat feim, const cv::Mat disp_img) {
-  glBegin(GL_POINTS);
+struct PCL {
+  cv::Vec3f pts;
+  cv::Vec3b clr;
+};
 
+void PointClouds(const cv::Mat& disp_img,
+                 const cv::Mat& feim,
+                 std::vector<PCL>& pcl_vec) {
   double fx = Kfe.at<double>(0,0);
   double fy = Kfe.at<double>(1,1);
   double cx = Kfe.at<double>(0,2);
   double cy = Kfe.at<double>(1,2);
-  double bl = cv::norm(Translation);
+  double bl = cv::norm(T);
+  double xi = xil.at<double>(0,0);
 
   double w_pi = rect_cols  / M_PI;
   double h_pi = rect_rows / M_PI;
@@ -246,25 +242,24 @@ void DrawScene(const cv::Mat feim, const cv::Mat disp_img) {
 
   for (int r = 0; r < feim.rows; ++r) {
     for (int c = 0; c < feim.cols; ++c) {
-      cv::Vec3b color = feim.at<cv::Vec3b>(r,c);
       if (hypot(c - cx, r - cy) > thr_now)
         continue;
 
-      double e = (c - cx) / fx;
-      double f = (r - cy) / fy;
-      double ef = e * e + f * f;
-      double zx = (xi + sqrt(1. + (1. - xi*xi) * ef)) / (ef + 1.);
+      double ee = (c - cx) / fx;
+      double ff = (r - cy) / fy;
+      double ef = ee * ee + ff * ff;
+      double zz = (xi + sqrt(1. + (1. - xi*xi) * ef)) / (ef + 1.);
 
-      if (isnan(zx))
+      if (isnan(zz))
         continue;
 
-      double xc = e * zx;
-      double yc = f * zx;
-      double zc = zx - xi;
+      double xc = zz * ee;
+      double yc = zz * ff;
+      double zc = zz - xi;
 
-      double tt = acos(-xc);
-      double pp = acos(-yc / hypot(yc, zc));
-      float disp = disp_img.at<float>((int) (pp * h_pi), (int) (tt * w_pi));
+      double tt   = acos(-xc);
+      double pp   = acos(-yc / hypot(yc, zc));
+      float  disp = disp_img.at<float>((int) (pp * h_pi), (int) (tt * w_pi));
 
       if (disp <= 0.f)
         continue;
@@ -272,15 +267,33 @@ void DrawScene(const cv::Mat feim, const cv::Mat disp_img) {
       double diff  = pi_w * disp;
       double depth = bl * sin(tt - diff) / sin(diff);
 
-      glColor3ub(color(2), color(1), color(0));
-      glVertex3f(xc * depth, yc * depth, zc * depth);
+      cv::Vec3b color = feim.at<cv::Vec3b>(r,c);
+
+      PCL pcl;
+      pcl.pts = cv::Vec3f(xc * depth, yc * depth, zc * depth);
+      pcl.clr = cv::Vec3b(color(2), color(1), color(0));
+      pcl_vec.push_back(pcl);
     }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void DrawScene(const std::vector<PCL>& pcl_vec) {
+  glBegin(GL_POINTS);
+
+  for (uint i = 0; i < pcl_vec.size(); ++i) {
+    PCL pcl = pcl_vec[i];
+    glColor3ub(pcl.clr(0), pcl.clr(1), pcl.clr(2));
+    glVertex3f(pcl.pts(0), pcl.pts(1), pcl.pts(2));
   }
 
   glEnd();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+
+bool live = false; // To run live mode, you need a CaliCam from www.astar.ai
 
 int main(int argc, char** argv) {
   std::string param_name = argc >= 2 ? argv[1] : "./astar_calicam.yml";
@@ -289,7 +302,8 @@ int main(int argc, char** argv) {
   LoadParameters(param_name);
   InitRectifyMap();
 
-  scene = new glwindow::SceneWindow(960, 720, "Panorama 3D Scene");
+  glwindow::SceneWindow* scene =
+      new glwindow::SceneWindow(960, 720, "Panorama 3D Scene");
 
   cv::VideoCapture vcapture;
   if (live) {
@@ -307,18 +321,16 @@ int main(int argc, char** argv) {
 
   std::string win_name = "Fisheye Image";
   cv::namedWindow(win_name);
-
-  if (cam_model == "stereo") {
-    cv::createTrackbar("Num Disp:  16 + 16 *", win_name,
-                       &ndisp_bar,  ndisp_max,   OnTrackNdisp);
-    cv::createTrackbar("Blk   Size :     5  +  2 *", win_name,
-                       &wsize_bar,  wsize_max,  OnTrackWsize);
-    cv::createTrackbar("Threshold :     200 + ", win_name,
-                       &thr_bar,    thr_max,    OnTrackThreshold);
-  }
+  cv::createTrackbar("Num Disp:  16 + 16 *", win_name,
+                     &ndisp_bar,  ndisp_max,   OnTrackNdisp);
+  cv::createTrackbar("Blk   Size :     5  +  2 * ", win_name,
+                     &wsize_bar,  wsize_max,  OnTrackWsize);
+  cv::createTrackbar("Threshold :     190 + ", win_name,
+                     &thr_bar,    thr_max,    OnTrackThreshold);
 
   cv::Mat raw_img = cv::imread(image_name, cv::IMREAD_COLOR);
   cv::Mat raw_imgl, raw_imgr, fe_img, ll_imgl, ll_imgr;
+
   while (1) {
     if (live)
       vcapture >> raw_img;
@@ -331,22 +343,24 @@ int main(int argc, char** argv) {
     raw_img(cv::Rect(        0, 0, img_width, cap_rows)).copyTo(raw_imgl);
     raw_img(cv::Rect(img_width, 0, img_width, cap_rows)).copyTo(raw_imgr);
 
-    cv::remap(raw_imgl, ll_imgl, smap[0][0], smap[0][1], cv::INTER_LINEAR);
-    cv::remap(raw_imgr, ll_imgr, smap[1][0], smap[1][1], cv::INTER_LINEAR);
+    cv::remap(raw_imgl, fe_img,  fmap[0],    fmap[1],    cv::INTER_LINEAR);
+    cv::remap(raw_imgl, ll_imgl, lmap[0][0], lmap[0][1], cv::INTER_LINEAR);
+    cv::remap(raw_imgr, ll_imgr, lmap[1][0], lmap[1][1], cv::INTER_LINEAR);
 
-    cv::remap(raw_imgl, fe_img, mmap[0], mmap[1], cv::INTER_LINEAR);
+    cv::Mat          disp_img;
+    std::vector<PCL> pcl_vec;
 
-    imshow(win_name, fe_img);
-
-    cv::Mat disp_img;
     DisparityImage(ll_imgl, ll_imgr, disp_img);
+    PointClouds(disp_img, fe_img, pcl_vec);
 
     if (scene->win.alive()) {
       if (scene->start_draw()) {
-        DrawScene(fe_img, disp_img);
+        DrawScene(pcl_vec);
         scene->finish_draw();
       }
     }
+
+    imshow(win_name, fe_img);
 
     char key = cv::waitKey(1);
     if (key == 'q' || key == 'Q' || key == 27)
@@ -354,7 +368,6 @@ int main(int argc, char** argv) {
   }
 
   delete scene;
-
   return 0;
 }
 
